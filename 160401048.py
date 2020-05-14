e406be2eb77703945436ac73527c2054f02a7e9d
BerkantDuman <dumanberkant562@gmail.com>
2020-05-14 14:28:17 +0300

    Berkant Duman 1604401048

--- 160401048.py
+++ 160401048.py
@@ -1,14 +1,82 @@
 #Berkant Duman No:160401048
 import random
-import functions
 import math
+
+
+def is_prime(n, k):
+    """bir sayının (kesin olmamakla birlikte) asal olup olmadığını kontrol eden fonksiyon (Miller-Rabin)"""
+    if n == 2 or n == 3:
+        return True 
+    if n <= 1 or n % 2 == 0:
+        return False
+
+    d = n - 1; 
+    while (d % 2 == 0): 
+        d //= 2; 
+
+    for i in range(k): 
+        if (not miller_test(d, n)): 
+            return False;  
+    
+    return True
+
+
+def miller_test(d, n):
+    a = random.randrange(2, (n-2))
+    x = pow(a, d, n)
+    if x == 1 or x == (n - 1): return True
+    
+    i = d 
+    while i != n - 1:
+        x = pow(x, 2, n)
+        i *=2
+        if (x == 1): return False
+        if (x == n - 1): return True
+    return False
+
+def generate_random_prime_numbers(bit):
+    """ Verilen bit uzunluğuna göre bir asal sayı oluşturur"""
+    while True:
+        number = random.getrandbits(int(bit))
+        if(is_prime(number,bit)):
+            return number
+
+def legendre_symbol(x, p):
+    """ a, p sayılarının legendre symbol değerlerini döndürür"""
+    ls = pow(x, (p - 1) // 2, p)
+    if(ls == 1): return 1
+    elif (ls == 0): return 0
+    else: return -1
+
+
+def quadratic_non_residue(p, q):
+    """ p ve q sayıları için legendre sembolü -1 olan ortak bir sayı döndürür"""
+    x = 0
+    while ((legendre_symbol(x, p) != -1) and (legendre_symbol(x, q) != -1)):
+        x = random.randint(1, p)
+
+    return x
+
+
+def string_to_bits(text):
+    """ Verilen stringi bitlere çevirir"""
+    return ''.join(format(ord(i), 'b').zfill(9) for i in text)
+
+
+
+def bits_to_string(bits):
+    """ Verilen bitleri stringe çevirir"""
+    string = ""
+    for i in range(0, len(bits), 9):
+        string += chr(int(bits[i:i+9],2))
+    return string
 
 
 
 def keygen(bit):
-    p = functions.generate_random_prime_numbers(bit)
-    q = functions.generate_random_prime_numbers(bit)
-    x = functions.quadratic_non_residue(p, q)
+    p = generate_random_prime_numbers(bit)
+    q = generate_random_prime_numbers(bit)
+    x = quadratic_non_residue(p, q)
     n = p * q
 
     public = open("publickey.txt","w")
@@ -28,7 +96,7 @@ def encrypt(plaintext, publickey):
         print("Anahtarlar bulunamadı, lütfen önce keygen fonksiyonunu çağırınız.")
         return
     readText = text.read()
-    bits = functions.string_to_bits(readText)
+    bits = string_to_bits(readText)
     lines = publicKey.readlines()
     
     x = int(lines[0][3:])
@@ -66,13 +134,13 @@ def decrypt(ciphertext, privatekey):
     
     decryptedBits = ""
     for c in encryptedBits:
-        if functions.legendre_symbol(int(c), p) == 1 and functions.legendre_symbol(int(c), q) == 1:
+        if legendre_symbol(int(c), p) == 1 and legendre_symbol(int(c), q) == 1:
             
             decryptedBits += "0"
         else:
             decryptedBits += "1"
     plainText2 = open ("plaintext2", "w")
-    plainText2.write(functions.bits_to_string(decryptedBits))
+    plainText2.write(bits_to_string(decryptedBits))
     plainText2.close()
 
     plainText = open("plaintext", "r")
@@ -85,6 +153,6 @@ def decrypt(ciphertext, privatekey):
 
 
 
-#keygen(int(input("Bit uzunluğunu giriniz:")))
-#encrypt("plaintext", "publickey.txt")
-#decrypt("ciphertext", "privatekey.txt")
+keygen(int(input("Bit uzunluğunu giriniz:")))
+encrypt("plaintext", "publickey.txt")
+decrypt("ciphertext", "privatekey.txt")
