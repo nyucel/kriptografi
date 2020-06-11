c21c1b4c27cdd2f5f9baafae3057264c9849003a
atakan <atakanhr@gmail.com>
2020-06-11 09:59:12 +0300

    v0.9.1
    hash kurcalaması

--- final/170401009/170401009.py
+++ final/170401009/170401009.py
@@ -15,76 +15,85 @@ import random
 import binascii
 import time
 
+
+#
 def string_to_int(data):
     return int(binascii.hexlify(data.encode('utf-8')).decode("ascii"),16) #byte değerini int değerine çeviriyor..
-
 def int_to_binary(data):
     return '{0:b}'.format(data)
 def binary_to_int(data):
     return int(data,2)
 def bytes_to_binary(data):
     return int_to_binary(string_to_int(data.decode()))
-
-
 def left_rotate(x, amount):
     x &= 0xFFFFFFFF
     return ((x<<amount) | (x>>(32-amount))) & 0xFFFFFFFF
 
-def final(dosya): #todo hash fonksiyonu değiştirilecek
+def final(dosya):
     # return '{:b}'.format(int(hashlib.md5(dosya).hexdigest(), 16)).zfill(128)
     magic_numbers = [0xFEE1DEAD, 0xBADDCAFE, 0xDEAD10CC, 0x8BADF00D]
     if type(dosya)!= bytes:
         dosya = dosya.encode()
-    uzunluk = len(bytes_to_binary(dosya))
+    dosya = binary_to_int(bytes_to_binary(dosya))
+    dosya = int_to_binary(dosya)
+    uzunluk = len(dosya)
     ekstra = uzunluk % 4    #4 parçaya bölünüyor mu
-    dosya = bytes_to_binary(dosya).zfill(ekstra)     #dosyayı 4 e bölünür hale getiriyorum
+    dosya = dosya.zfill(ekstra)     #dosyayı 4 e bölünür hale getiriyorum
     parcalar=[[],[],[],[]]
     bit_toplamı=0
-    print(uzunluk)
-    print(ekstra)
-    print(dosya)
-    print(parcalar)
+    # print(uzunluk)
+    # print(ekstra)
+    # print(dosya)
+
+    sira = 0
     for i in dosya:
         bit_toplamı+=int(i)
-        if len(parcalar[0]) <= uzunluk/4:
-            print(len(parcalar[0]))
-            parcalar[0].append(str(i))
-        elif len(parcalar[2]) <= uzunluk/4:
-            parcalar[2].append(str(i))
-        elif len(parcalar[3]) <= uzunluk/4:
-            parcalar[3].append(str(i))
-        else:
-            parcalar[1].append(str(i))
-    print(parcalar)
+        parcalar[3-sira%4].append(str(i))
+        sira +=1
+        # if len(parcalar[3]) <= uzunluk/4:
+        #     # print(len(parcalar[0]))
+        #     parcalar[3].append(str(i))
+        # elif len(parcalar[2]) <= uzunluk/4:
+        #     parcalar[2].append(str(i))
+        # elif len(parcalar[0]) <= uzunluk/4:
+        #     parcalar[0].append(str(i))
+        # else:
+        #     parcalar[1].append(str(i))
+    # print(parcalar)
     parca1="".join(parcalar[0])
     parca2="".join(parcalar[1])
     parca3="".join(parcalar[2])
     parca4="".join(parcalar[3])
-    print(parca1,parca2,parca3,parca4)
-    parca1 = binary_to_int(parca1) ^ magic_numbers[0]
-    print(parca1)
-    parca1 >>=bit_toplamı % 7
-    print(parca1)
+    # print(parca1,parca2,parca3,parca4)
+    parca1 = binary_to_int(parca1) + magic_numbers[0]
+    parca1 = parca1 | magic_numbers[0]
+    # print(parca1)
+    parca1 >>=(bit_toplamı % 5)
+    parca1 = parca1 | magic_numbers[3]
+    # print(parca1)
     parca2 = binary_to_int(parca2) ^ magic_numbers[1]
     dosya = parca1+parca2
-    dosya << 42
-    print(dosya)
+    dosya = left_rotate(dosya,11)
+    dosya = ~dosya
+    # print(dosya)
     parca3 = binary_to_int(parca3) | magic_numbers[2]
     dosya = dosya+~parca3
-    print(dosya)
+    # print(dosya)
     parca4 = binary_to_int(parca4) & magic_numbers[3]
     parca4 <<=bit_toplamı % 21
-    print(parca4)
+    # print(parca4)
     dosya = dosya ^ parca4
-    print(dosya)
-    dosya = dosya % 2**32
-    print(dosya)
+    # print(dosya)
+
+
+    dosya = dosya % 2**32 #32 bit
+    # print(dosya)
     # dosya = int_to_binary(dosya)
     # print(dosya)
     # dosya=dosya.zfill(32) # 32 bite tamamlıyoruz
     return dosya
 
-class blockchain: #todo burayı tamamla
+class blockchain:
     def __init__(self,baslangic=1,bitis=100):
         self.baslangic = baslangic
         self.bitis = bitis
@@ -95,8 +104,25 @@ class blockchain: #todo burayı tamamla
             temp.calistir()
             print("{} OK!".format(i))
 
-    def kontrol_et(self): #todo zincirde bozulma olup olmadığını kontrol ediyor.
+    def kontrol_et(self):
         pass
+    def cakisma_kontrol(self):
+        hashler=[]
+        for i in range(1,101):
+            dosya_ismi = str(i).zfill(3)+".txt"
+            f = open(dosya_ismi,"rb")
+            a =f.read()
+            f.close()
+            hashler.append(hashlib.md5(a).hexdigest()) #çivi çiviyi söker
+        # print(hashler)
+
+        setOfElems = set()
+        for elem in hashler:
+            if elem in setOfElems:
+                print("ÇAKIŞMA VAR {}".format(elem))
+            else:
+                setOfElems.add(elem)
+        print("HİÇ ÇAKIŞMA BULUNAMADI")
 
 
 class blok:
@@ -142,13 +168,16 @@ class blok:
         f = open("HASHSUM", "a")
         if int(self.blok_numarası) == 2:
             f.write("BLOK | RASTGELE SAYI | BLOĞUN KENDİ HASHI\n")
-        f.write("{} | {} | {} \n".format(self.blok_numarası, '{:b}'.format(self.rastgele).zfill(32), '{:b}'.format(self.blokhash).zfill(32))) #todo hash fonksiyonunu değiştirince zfilleri değiştir.
+        f.write("{} | {} | {} \n".format(self.blok_numarası, '{:b}'.format(self.rastgele).zfill(32), '{:b}'.format(self.blokhash).zfill(32)))
         f.close()
-
+        f = open("DEBUG", "a")
+        f.write("{}\n".format('{:b}'.format(self.blokhash).zfill(32)))
 
 if __name__ == "__main__":
      bchain = blockchain()
      bchain.zincirle()
+     bchain.cakisma_kontrol()
+
      # a = open("001.txt", "rb")
      # a = a.read()
      # final(a)
