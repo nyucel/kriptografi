be29a380675f28c5adc08aff3ba3a314f005d83e
atakan <atakanhr@gmail.com>
2020-06-11 20:56:06 +0300

    v1.0-170401009
    Beta version Zamanlayıcı Eklendi.

--- final/170401009/170401009.py
+++ final/170401009/170401009.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 #170401009 ATAKAN TÜRKAY
 
+#Notlarım
 # x = txt.zfill(10) # 10 karakter uzunluğunda 0 döşemek için
 # bin(10) binary halini görmek için
 # '{0:b}'.format(10)   binary halini görmek için
-
-
 # '{0:b}'.format(random.randint(2**31,2**32-1))  #32 bitlik rastgele bir sayı oluşturuyor.
 # '{:b}'.format(int(hashlib.md5("ata".encode()).hexdigest(),16)).zfill(128) hex->int->binary
-#   '{0:x}'.format(122545).zfill(32)
+#  '{0:x}'.format(122545).zfill(32)
+
 import hashlib
 import random
 import binascii
@@ -50,20 +50,12 @@ def final(dosya):
         bit_toplamı+=int(i)
         parcalar[3-sira%4].append(str(i))
         sira +=1
-        # if len(parcalar[3]) <= uzunluk/4:
-        #     # print(len(parcalar[0]))
-        #     parcalar[3].append(str(i))
-        # elif len(parcalar[2]) <= uzunluk/4:
-        #     parcalar[2].append(str(i))
-        # elif len(parcalar[0]) <= uzunluk/4:
-        #     parcalar[0].append(str(i))
-        # else:
-        #     parcalar[1].append(str(i))
     # print(parcalar)
     parca1="".join(parcalar[0])
     parca2="".join(parcalar[1])
     parca3="".join(parcalar[2])
     parca4="".join(parcalar[3])
+
     # print(parca1,parca2,parca3,parca4)
     parca1 = binary_to_int(parca1) + magic_numbers[0]
     parca1 = parca1 | magic_numbers[0]
@@ -73,8 +65,8 @@ def final(dosya):
     # print(parca1)
     parca2 = binary_to_int(parca2) ^ magic_numbers[1]
     dosya = parca1+parca2
-    dosya = left_rotate(dosya,11)
-    dosya = ~dosya
+    dosya = left_rotate(dosya,3)
+    # dosya = ~dosya
     # print(dosya)
     parca3 = binary_to_int(parca3) | magic_numbers[2]
     dosya = dosya+~parca3
@@ -84,6 +76,44 @@ def final(dosya):
     # print(parca4)
     dosya = dosya ^ parca4
     # print(dosya)
+
+    if dosya < 0:
+        dosya = ~dosya
+    dosya = int_to_binary(dosya)
+    # print(dosya)
+    for i in dosya: #tekrar karıştır
+        if len(parcalar[3]) <= uzunluk/4:
+            # print(len(parcalar[0]))
+            parcalar[3].append(str(i))
+        elif len(parcalar[2]) <= uzunluk/4:
+            parcalar[2].append(str(i))
+        elif len(parcalar[0]) <= uzunluk/4:
+            parcalar[0].append(str(i))
+        else:
+            parcalar[1].append(str(i))
+    parca1 = "".join(parcalar[0])
+    parca2 = "".join(parcalar[1])
+    parca3 = "".join(parcalar[2])
+    parca4 = "".join(parcalar[3])
+    parca1 = binary_to_int(parca1) + magic_numbers[0]
+    parca1 = parca1 | magic_numbers[0]
+    # print(parca1)
+    parca1 >>= (bit_toplamı % 5)
+    parca1 = parca1 | magic_numbers[3]
+    # print(parca1)
+    parca2 = binary_to_int(parca2) ^ magic_numbers[1]
+    dosya = parca1 + parca2
+    dosya = left_rotate(dosya, 11)
+    dosya = ~dosya
+    # print(dosya)
+    parca3 = binary_to_int(parca3) | magic_numbers[2]
+    dosya = dosya + ~parca3
+    # print(dosya)
+    parca4 = binary_to_int(parca4) & magic_numbers[3]
+    parca4 <<= bit_toplamı % 21
+    # print(parca4)
+    dosya = dosya ^ parca4
+    # dosya = binary_to_int(parca1)+binary_to_int(parca2)+binary_to_int(parca3)+binary_to_int(parca4)
 
 
     dosya = dosya % 2**32 #32 bit
@@ -122,14 +152,14 @@ class blockchain:
                 print("ÇAKIŞMA VAR {}".format(elem))
             else:
                 setOfElems.add(elem)
-        print("HİÇ ÇAKIŞMA BULUNAMADI")
+        print("1-100 TXT ARASINDA HİÇ ÇAKIŞMA BULUNAMADI")
 
 
 class blok:
     def __init__(self, blok_numarası):
         self.blok_numarası = str(blok_numarası).zfill(3)
         self.uzanti = "txt"
-        self.rastgele = random.randint(2 ** 31, 2 ** 32 - 1)  # 32 bitlik rastgele bir sayı oluşturuyor.
+        self.rastgele = 0
         self.oncekibloknumara = str(blok_numarası - 1).zfill(3)
         self.oncekiblokhash = 0
         self.blokhash = 0
@@ -158,11 +188,47 @@ class blok:
         f.close()
         self.blokhash = final(a)
 
+    def rastgele_sayi(self):
+        # self.rastgele = random.randint(2 ** 31, 2 ** 32 - 1)  # 32 bitlik rastgele bir sayı oluşturuyor.
+        self.rastgele = random.getrandbits(32)
+        return self.rastgele
+
     def blok_kaydet(self):
         f = open("{}.{}".format(self.blok_numarası, self.uzanti), "w")
-        toplam = self.oncekiblokhash + self.rastgele
+        toplam = self.oncekiblokhash + self.rastgele_sayi()
+        print(toplam)
         f.write('{:b}'.format(toplam).zfill(32))
         f.close()
+        ZAMANLAYICI = time.time()
+        while (ZAMANLAYICI - time.time()) < 600: #KODUN ZAMAN HARCAYAN KISMI BURASI. 10 DAKİKAYI AŞARSA KAPATIYOR.
+            f = open("{}.{}".format(self.blok_numarası, self.uzanti), "rb")
+            a = f.read()
+            f.close()
+            # print(a)
+            print('{:b}'.format(final(a)).zfill(32)[:8])
+            # time.sleep(1)
+            if '{:b}'.format(final(a)).zfill(32)[:8] != "00000000":
+                f = open("{}.{}".format(self.blok_numarası, self.uzanti), "rb")
+                a = f.read()
+                f.close()
+                print('*******************************************')
+                print("HATALI HASH")
+                print('DOSYA HASH =  '+'{:b}'.format(final(a)).zfill(32))
+                toplam = self.oncekiblokhash + self.rastgele_sayi()
+                f = open("{}.{}".format(self.blok_numarası, self.uzanti), "w")
+                f.write('{:b}'.format(toplam).zfill(32))
+                f.close()
+
+                print('TOPLAM    =   '+'{:b}'.format(toplam).zfill(32))
+                print('ONCEKI_HASH   '+'{:b}'.format(self.oncekiblokhash).zfill(32))
+                print('RASTGELE SAYI '+'{:b}'.format(self.rastgele).zfill(32))
+                print('*******************************************')
+            else:
+                break
+        else : #WHILE - ELSE  | EĞER ZAMAN AŞIMINA UĞRAYIP ÇIKARSA BURAYA GİRİYOR.
+            print("SÜRE AŞIMINA UĞRADI 10 DAKİKA GEÇTİ . ÇIKILIYOR")
+            exit(-1)
+
 
     def hashsum_kaydet(self):
         f = open("HASHSUM", "a")
@@ -171,6 +237,7 @@ class blok:
         f.write("{} | {} | {} \n".format(self.blok_numarası, '{:b}'.format(self.rastgele).zfill(32), '{:b}'.format(self.blokhash).zfill(32)))
         f.close()
         f = open("DEBUG", "a")
+        f.write("-----hashlerin aynı olup olmadığını duplicated lane varmı diye elle kontrol ediyorum") #todo kaldırılacak
         f.write("{}\n".format('{:b}'.format(self.blokhash).zfill(32)))
 
 if __name__ == "__main__":
@@ -181,3 +248,4 @@ if __name__ == "__main__":
      # a = open("001.txt", "rb")
      # a = a.read()
      # final(a)
+
